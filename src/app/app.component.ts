d1deec1e1612e91cd82b22c9dd546f3587c5e800
dalton-x <daltonx43@gmail.com>
2022-10-02 19:50:15 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'newportefolio';
 
   scrollToId(id){
     document.querySelector("#" + id).scrollIntoView({behavior: "smooth"});
+  }
+  public loading = false;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.loading = true
+    }, 3500);
   }
 }
