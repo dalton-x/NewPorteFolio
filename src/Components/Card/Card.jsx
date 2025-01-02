818966e8f3e5da56b0486a1b8d8e184ee8f11381
thomas_girard45 <54374885+dalton-x@users.noreply.github.com>
2025-01-02 22:31:34 +0100

    Ajout des projets de la formation dev web en json

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,22 +2,34 @@
 export const Card = ({ card }) => {  
   return (
     <div className="col-lg-6 mt-md-3">
-      <div className="card">
+      <div className="card h-100">
         <div className="card-header">
           <h5>{card.header}</h5>
         </div>
         <div className="card-body">
           <h6>{card.body.title}</h6>
-          <ul>
-            {card.body.items.map((item, index) => (
-              <li key={index}>{item}</li>
-            ))}
-          </ul>
+            <ul>
+              {card.body.items.map((item, index) => (
+                <li key={'item_'+index} dangerouslySetInnerHTML={{ __html: item }} />
+              ))}
+            </ul>
         </div>
         <div className="card-footer d-grid gap-2">
-          <a target="_blank" href={card.footer.link.url} className="btn btn-primary">
-            {card.footer.link.text}
-          </a>
+          {
+            (card.footer.link != undefined)
+            ?
+              <a target="_blank" href={card.footer.link.url} className="btn btn-primary">
+                {card.footer.link.text}
+              </a>
+            :  
+              <div className="row">
+                {card.footer.btn.map((btn, index) => (
+                  <div key={'btn_'+index} className="col-6 d-grid gap-2">
+                    <button className="btn btn-primary" data-bs-target={btn.target} data-bs-toggle="modal">{btn.text}</button>
+                  </div>
+                ))}
+              </div>
+          }
         </div>
       </div>
     </div>
--- src/Components/Projets/DevWeb/DevWeb.jsx
+++ src/Components/Projets/DevWeb/DevWeb.jsx
@@ -1,149 +1,18 @@
 
 import ModalIframe from '../../Modal/ModalIframe';
 import './Style.css'
+import dataCard from "./projets.json";
+import { Card } from "../../Card/Card";
 
 
 function DevWeb() {
   return (
     <section className="py-5">
       <div className="container">
-        <div className="row m-3">
-          <div className="col-lg-6 mt-2">
-            <div className="card h-100 shadow-lg">
-              <div className="card-header text-center text-uppercase">
-                Mise en forme d&apos;un site de menus de restaurants
-              </div>
-              <div className="card-body">
-                <h5 className="card-title text-center">Les animations et Sass</h5>
-                <ul className="card-text p-2">
-                  <li>
-                    En partant de maquette fournit par un client, j&apos;ai dû réaliser le site web avec un cahier des charges définit.
-                  </li>
-                  <li>
-                    La création d&apos;animations et de mise en forme des menus fournit en images pour les rendre au format. <i className="fab fa-html5 text-danger" title="HTML5"></i> et <i className="fab fa-css3-alt text-info" title="CSS3"></i>.
-                  </li>
-                </ul>
-              </div>
-              <div className="card-footer d-grid gap-2">
-                <a target="_blank" rel="noopener" href="https://projet-3.dev-web.fr/" className="btn btn-primary btn-block">Voir le site</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-6 mt-2">
-            <div className="card h-100 shadow-lg">
-              <div className="card-header text-center text-uppercase">
-                Reprise d&apos;un site pour le mettre aux normes actuelles
-              </div>
-              <div className="card-body">
-                <h5 className="card-title text-center">L&apos;optimisation et le référencement</h5>
-                <ul className="card-text p-2">
-                  <li>
-                    Le client a un site web n&apos;étant pas très bien référencé dans les moteurs de recherche.
-                  </li>
-                  <li>
-                    Je me devais de remettre le site web aux normes actuelles et surtout d&apos;enlever toutes les mauvaises pratiques de référencement.
-                  </li>
-                </ul>
-              </div>
-              <div className="card-footer row ">
-                <div className="col-6 d-grid gap-2">
-                  <button className="btn btn-primary" data-bs-target="#modalSEOOriginal" data-bs-toggle="modal">SEO original</button>
-                </div>
-                <div className="col-6 d-grid gap-2">
-                  <button className="btn btn-primary" data-bs-target="#modalSEOOptimise" data-bs-toggle="modal">SEO optimisé</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row m-3">
-          <div className="col-lg-6 mt-2">
-            <div className="card h-100 shadow-lg">
-              <div className="card-header text-center text-uppercase">
-                Utilisation d&apos;une API serveur
-              </div>
-              <div className="card-body">
-                <h5 className="card-title text-center">Développement du front-end</h5>
-                <ul className="card-text p-2">
-                  <li>
-                    Le client m&apos;a fourni une API pour se connecter a son serveur de son site e-commerce.
-                  </li>
-                  <li>
-                    J&apos;ai dû faire toute l&apos;interface client en respectant le CDC et le respect de son interface client-serveur.
-                  </li>
-                  <li>
-                    Utilisation de <i className="fab fa-html5 text-danger" title="HTML5"></i> , <i className="fab fa-css3-alt text-info" title="CSS3"></i> et <i className="fab fa-js-square text-warning"></i>
-                  </li>
-                </ul>
-              </div>
-              <div className="card-footer d-grid gap-2">
-                <a target="_blank" rel="noopener" href="https://projet-5.dev-web.fr/" className="btn btn-primary btn-block">Voir le site</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-6 mt-2">
-            <div className="card h-100 shadow-lg">
-              <div className="card-header text-center text-uppercase">
-                Création du serveur avec un front déjà existant
-              </div>
-              <div className="card-body">
-                <h5 className="card-title text-center">Développement du back-end</h5>
-                <ul className="card-text p-2">
-                    <li>
-                      Le client m&apos;a demandé de faire le serveur de son application de gestion de sauces piquantes.
-                    </li>
-                    <li>
-                      Je devais reprendre son API et faire le traitement des données :
-                      <ul>
-                        <li>
-                          Utilisateurs : inscription/Connexion.
-                        </li>
-                        <li>
-                        Traitement de l&apos;ajout / suppression des sauces piquantes.
-                        </li>
-                        <li>
-                        Gestion des <i className="fas fa-thumbs-up text-success" title="like"></i> & <i className="fas fa-thumbs-down text-danger" title="unlike"></i> des sauces par les utilisateurs.
-                        </li>
-                      </ul>
-                    </li>
-                    <li>
-                      Utilisation de <i className="fab fa-node text-success" title="nodeJS"></i> & de MongoDB
-                    </li>
-                </ul>
-              </div>
-              <div className="card-footer d-grid gap-2">
-                <a target="_blank" rel="noopener" href="https://projet-6.dev-web.fr/" className="btn btn-primary btn-block">Voir le site</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row m-3">
-          <div className="col-lg-6 mt-2 offset-lg-3">
-            <div className="card h-100 shadow-lg">
-              <div className="card-header text-center text-uppercase">
-                Création d&apos;un réseau social
-              </div>
-              <div className="card-body">
-                <h5 className="card-title text-center">Groupomania</h5>
-                <ul className="card-text p-2">
-                  <li>
-                    Le client m&apos;a contacté pour lui faire un réseau social pour son entreprise en plein développement.
-                  </li>
-                  <li>
-                    Avec un CDC rigoureux et l&apos;utilisation des logos de l&apos;entreprise.
-                  </li>
-                  <li>
-                    L&apos;utilisation du framework <i className="fab fa-angular text-danger"></i> pour le front et de <i className="fab fa-node text-success" title="nodeJS"></i> avec MYSQL et sequelize pour la gestion des données serveur.
-                  </li>
-                </ul>
-              </div>
-              <div className="card-footer d-grid gap-2">
-                <a target="_blank" rel="noopener" href="https://projet-7.dev-web.fr/" className="btn btn-primary btn-block">Voir le site</a>
-              </div>
-            </div>
-          </div>
+        <div className="row m-3 justify-content-center">
+            {dataCard.map((card, index) => (
+              <Card key={'card_' + index} card={card} />
+            ))}
         </div>
 
         <ModalIframe
